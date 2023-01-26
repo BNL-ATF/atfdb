e9b4e268d5e61ca3a15871f9c937839d5bbd6d34
Maksim Rakitin <mrakitin@bnl.gov>
2023-01-25 19:15:30 -0500

    TST: add pytest tests with the session-scoped server fixture

--- atfdb/tests/test_atfdb.py
+++ atfdb/tests/test_atfdb.py
@@ -1,3 +1,13 @@
-def test_socket_server():
-    "Check that one and one are indeed two."
-    assert 1 + 1 == 2
+from atfdb.atfdb import get_channel_index, get_real, put_real
+
+
+def test_socket_read(socket_server):
+    chidx = get_channel_index("RT_DATABASE::PTEN20;RAS;RB_CURRENT_SETPT")
+    val = get_real(chidx)
+
+    assert type(val) is float
+
+
+def test_socket_write(socket_server):
+    chidx = get_channel_index("RT_DATABASE::PTEN20;CDS;SET_CURRENT_SETPT")
+    put_real(chidx, 100)
